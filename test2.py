5d609ee2757ac7d728750ef302f9ed745fffd9ad
Lim Khai Jun <limkhaijun@gmail.com>
2023-09-19 20:32:56 +0800

    Update test2.py
    another version of the work

--- test2.py
+++ test2.py
@@ -1,109 +1,129 @@
-import cv2 as cv
+import cv2
 import numpy as np
 import csv
-import math
-import time
+import matplotlib.pyplot as plt
 
-# Parameters for Shi-Tomasi corner detection
-feature_params = dict(maxCorners=300, qualityLevel=0.2, minDistance=2, blockSize=7)
-# Parameters for Lucas-Kanade optical flow
-lk_params = dict(winSize=(15, 15), maxLevel=2, criteria=(cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
+# Initialize video capture
+cap = cv2.VideoCapture('shibuya.mp4')
 
-# The video file path
-video_path = "shibuya.mp4"
+# Create a VideoWriter object to save the output
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
 
-cap = cv.VideoCapture(video_path)
-color = (0, 255, 0)
-ret, first_frame = cap.read()
-prev_gray = cv.cvtColor(first_frame, cv.COLOR_BGR2GRAY)
-prev = cv.goodFeaturesToTrack(prev_gray, mask=None, **feature_params)
-mask = np.zeros_like(first_frame)
+# Lucas-Kanade parameters
+lk_params = dict(winSize=(15, 15), maxLevel=2, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
-# Create a CSV file for writing
-csv_filename = "optical_flow_data.csv"
-csv_file = open(csv_filename, 'w', newline='')
-csv_writer = csv.writer(csv_file)
-csv_writer.writerow(["Frame", "Point_X", "Point_Y", "Flow_X", "Flow_Y"])
+# Create some random colors for drawing optical flow
+color = np.random.randint(0, 255, (100, 3))
 
-def angle_to_direction(angle):
-    if angle >= -45 and angle < 45:
-        return "East"
-    elif angle >= 45 and angle < 135:
-        return "South"
-    elif angle >= -135 and angle < -45:
-        return "North"
-    else:
-        return "West"
+# Read the first frame
+ret, old_frame = cap.read()
+old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
 
+# Initialize variables for tracking speed and direction
+average_speed_per_second = []
+average_speed_per_minute = []
 frame_count = 0
-start_time = time.time()
-total_flow_x = 0
-total_flow_y = 0
+fps = cap.get(cv2.CAP_PROP_FPS)
 
-frame_interval = 1.0  # Time interval between frames in seconds
-pixels_to_meters = 0.01  # Scaling factor (adjust according to your scene)
+# Initialize feature points for optical flow (p0)
+# You can use a feature detection method (e.g., Shi-Tomasi or Harris) to initialize p0
+p0 = cv2.goodFeaturesToTrack(old_gray, maxCorners=100, qualityLevel=0.3, minDistance=7, blockSize=7)
 
-# Output video writer
-output_video_path = "output_video.avi"
-fourcc = cv.VideoWriter_fourcc(*'XVID')
-output_video = cv.VideoWriter(output_video_path, fourcc, 30.0, (500, 500))  # Set resolution to 500x500
+# Check if any feature points were found
+if p0 is None or len(p0) == 0:
+    print("No feature points found in the first frame.")
+    exit()
 
-while cap.isOpened():
-    ret, frame = cap.read()
+# Ensure data type and shape of p0 are as expected
+p0 = p0.astype(np.float32)
+p0 = p0.reshape(-1, 1, 2)
 
-    if frame is None:
-        break
+# Initialize mask for drawing optical flow vectors and circles
+mask = np.zeros_like(old_frame)
 
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    prev = cv.goodFeaturesToTrack(prev_gray, mask=None, **feature_params)
-    next, status, error = cv.calcOpticalFlowPyrLK(prev_gray, gray, prev, None, **lk_params)
-    good_old = prev[status == 1].astype(int)
-    good_new = next[status == 1].astype(int)
+# Histogram for flow angles
+angle_hist = []
 
-    for i, (new, old) in enumerate(zip(good_new, good_old)):
-        a, b = new.ravel()
-        c, d = old.ravel()
-        mask = cv.line(mask, (a, b), (c, d), color, 2)
-        frame = cv.circle(frame, (a, b), 3, color, -1)
+# CSV file for data logging
+csv_filename = 'crowd_flow_data.csv'
 
-        flow_x = a - c
-        flow_y = b - d
+with open(csv_filename, mode='w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(['Time (s)', 'Average Speed (px/s)', 'Dominant Direction'])
 
-        total_flow_x += flow_x
-        total_flow_y += flow_y
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-        csv_writer.writerow([frame_count, a, b, flow_x, flow_y])
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    elapsed_time = time.time() - start_time
-    if elapsed_time >= frame_interval:
-        average_flow_magnitude = math.sqrt(total_flow_x ** 2 + total_flow_y ** 2)
-        average_speed = (average_flow_magnitude * pixels_to_meters) / frame_interval
-        average_flow_direction = math.degrees(math.atan2(total_flow_y, total_flow_x))
-        average_direction_text = angle_to_direction(average_flow_direction)
-        print(f"Frame {frame_count}: Avg Speed = {average_speed:.2f} meters/second, Avg Direction = {average_direction_text}")
+        # Calculate optical flow using Lucas-Kanade
+        p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
 
-        avg_flow_text = f"Avg Speed: {average_speed:.2f} m/s, Avg Direction: {average_direction_text}"
-        frame = cv.putText(frame, avg_flow_text, (10, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+        # Select good points
+        good_new = p1[st == 1]
+        good_old = p0[st == 1]
 
-        start_time = time.time()
-        total_flow_x = 0
-        total_flow_y = 0
+        # Calculate the flow vectors and speeds
+        flow_vectors = good_new - good_old
+        speeds = np.linalg.norm(flow_vectors, axis=1)
+        average_speed = np.mean(speeds)
 
-    output = cv.add(frame, mask)
-    output_video.write(output)  # Write the frame to the output video without resizing
-    prev_gray = gray.copy()
-    prev = good_new.reshape(-1, 1, 2)
-    cv.imshow("sparse optical flow", output)
+        # Calculate the flow angles
+        angles = np.arctan2(flow_vectors[:, 1], flow_vectors[:, 0])
+        angle_hist.extend(angles)
 
-    if cv.waitKey(10) & 0xFF == ord('q'):
-        break
+        # Update frame count
+        frame_count += 1
 
-    frame_count += 1
+        # Append the speed to per-second and per-minute lists
+        average_speed_per_second.append(average_speed)
+        if frame_count % int(fps) == 0:
+            average_speed_per_minute.append(np.mean(average_speed_per_second))
+            # Calculate dominant direction based on the histogram
+            hist, bin_edges = np.histogram(angle_hist, bins=20, range=(-np.pi, np.pi))
+            dominant_angle = bin_edges[np.argmax(hist)]
+            dominant_direction = "North"  # Modify based on the dominant angle
+            angle_hist = []  # Reset histogram
 
-# Release the output video writer
-output_video.release()
+            # Write data to CSV file
+            elapsed_time = frame_count / fps
+            csv_writer.writerow([elapsed_time, np.mean(average_speed_per_minute), dominant_direction])
+            average_speed_per_second = []
 
-# Close the CSV file and release resources
-csv_file.close()
+        # Draw optical flow vectors on the frame
+        for i, (new, old) in enumerate(zip(good_new, good_old)):
+            a, b = new.ravel().astype(int)  # Convert to integers
+            c, d = old.ravel().astype(int)  # Convert to integers
+            mask = cv2.line(mask, (a, b), (c, d), color[i].tolist(), 2)
+            frame = cv2.circle(frame, (a, b), 5, color[i].tolist(), -1)
+
+        # Combine the frame and the mask
+        img = cv2.add(frame, mask)
+
+        # Display the frame
+        cv2.imshow('Crowd Flow', img)
+
+        # Write the frame to the output video
+        out.write(img)
+
+        # Update the previous frame and points
+        old_gray = frame_gray.copy()
+        p0 = good_new.reshape(-1, 1, 2)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+# Release resources
 cap.release()
-cv.destroyAllWindows()
+out.release()
+cv2.destroyAllWindows()
+
+# Plot the histogram
+plt.hist(angle_hist, bins=20, range=(-np.pi, np.pi))
+plt.title("Flow Angle Histogram")
+plt.xlabel("Angle (radians)")
+plt.ylabel("Frequency")
+plt.show()
